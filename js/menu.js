8e3fd855b8ae9c25a2ba98305fe8ca932b68d8c9
Braian <braianfelipecsk123@hotmail.com>
2022-03-09 11:07:50 -0300

    finalização da navbar

---
+++ js/menu.js
@@ -0,0 +1,30 @@
+//scripts para o menu hamburger
+let burger = document.querySelector('.hamburger');
+let menu = document.querySelector('.menu');
+let links = document.querySelectorAll('a');
+let bg = document.querySelector('.bg')
+burger.addEventListener('click',()=>{
+    handleCloseMenu()
+});
+
+links.forEach(link =>{
+    link.addEventListener('click',()=>{
+        handleCloseMenu()
+    })
+})
+
+bg.addEventListener('click',()=>{
+    handleCloseMenu()
+})
+
+const handleCloseMenu = () =>{
+    burger.classList.toggle('is-active');
+  menu.classList.toggle('menu-active');
+  if(menu.classList.contains('menu-active')){
+    document.body.style.overflow = 'hidden';
+    bg.classList.add('bg-active')
+  }else{
+    document.body.style.overflow = 'auto';
+    bg.classList.remove('bg-active')
+  }
+}
