fb2c7cd018c7cc03790884a1002ea8ce34c1d0fc
Braian <braianfelipecsk123@hotmail.com>
2022-03-10 12:34:50 -0300

    atualização

--- js/slide.js
+++ js/slide.js
@@ -20,4 +20,37 @@ const swiper = new Swiper('.swiper', {
     },
   
   });  
+
+  const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    centeredSlide:false,
+    autoplay: false,
+    breakpoints: {
+
+      1400: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+      },
+      880: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+  });  
+ 
  
